0a0fc23d5bfab7d5fe13fd4755a2ae278e961986
Samuel A. Idowu <idowusam.a@gmail.com>
2019-04-04 13:31:27 +0100

    new update

--- Server.js
+++ Server.js
@@ -12,3 +12,28 @@ function min(a, b) {
 	Math.min (a, b)
 }
 console.log(min(4, -4))
+
+function subtract(a,b) {
+	return subtract = a-b
+}
+console.log(subtract(14, 9))
+
+
+for ( let number = 1; number <= 100; ++number) {
+	 	if (!((number % 3 == 0) || (number % 5 == 0))) {
+	 		console.log(number)
+	 	}
+	 	
+
+	 	if ((number % 3 == 0) && (number % 5 == 0)) {
+	 			console.log("fizzbuzz")
+	 	}
+	 	else if (number % 3 == 0) {
+	 		console.log("fizz")
+	 	}
+	 	else if (number % 5 == 0) {
+	 			console.log("buzz")
+	 	}
+	 	
+}
+
