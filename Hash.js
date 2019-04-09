7293d07826b4ec34100110dce8c7c868462190b6
Samuel A. Idowu <idowusam.a@gmail.com>
2019-04-09 10:59:35 +0100

    try  hash code

---
+++ Hash.js
@@ -0,0 +1,21 @@
+	for (let line = "#"; line < 8; line += "#") {
+		line += "\n"
+		console.log(line);
+	}
+
+	let size = 8;
+
+	let board = ""
+
+	for (let y = 0; y < size; ++ y) {
+		for (let x = 0; x  < size; ++ x) {
+			if ((x + y) % 2 == 0) {
+				board += " ";
+			}
+			else {
+				board += "#";
+			}
+		}
+		board += "\n"
+	}
+	console.log(board)
