cf24457ec41dd4dbf16fa8ee56cadf26385c47a8
Samuel A. Idowu <idowusam.a@gmail.com>
2019-04-09 11:29:17 +0100

    commit for loop

--- counter.js
+++ counter.js
@@ -94,6 +94,20 @@ function wordCount (str) {
 console.log(wordCount("written test result"))
 
 
+for ( var start = 1; start <=5; ++start) {
+		console.log(start)
+
+		
+		if (start % 2 == 0){
+			square = start * start
+			console.log(square)
+
+		}
+	
+
+	}
+
+
 </script>
 </body>
 </html>
